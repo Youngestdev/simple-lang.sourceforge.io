5d79c80e4b9a4b290409dd5371f25c4bcd07fa3c
Thecarisma <azeezadewale98@gmail.com>
2018-03-08 14:39:00 +0100

    start rewrite in simple

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   // Hide div 2 by default
   $('#try_online_shell').hide();
 
+  Console.log($('#shell_hero').val());
   $('#shell_hero').click(function(){ 
       $('#introduction').hide();
       $('#try_online_shell').show();
